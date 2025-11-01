5846ce60020f97665b755fd6bbf3680ee3dae525
Chris Snell <1072626+chrissnell@users.noreply.github.com>
2025-10-31 22:27:12 -0600

    fix: add proto def'n

--- build.rs
+++ build.rs
@@ -4,8 +4,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(false)
         .file_descriptor_set_path("target/snowgauge_descriptor.bin")
         .compile_protos(
-            &["../protocols/snowgauge/snowgauge.proto"],
-            &["../protocols/snowgauge"],
+            &["proto/snowgauge.proto"],
+            &["proto"],
         )?;
     Ok(())
 }
